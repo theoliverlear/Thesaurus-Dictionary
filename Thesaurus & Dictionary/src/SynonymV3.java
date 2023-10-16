cfe0e06b67b84c1793a11bfb411c10fef41f5eb5
theoliverlear <sigwartholiver2@gmail.com>
2023-10-15 21:33:11 -0500

    Add normalizing of words

--- Thesaurus & Dictionary/src/SynonymV3.java
+++ Thesaurus & Dictionary/src/SynonymV3.java
@@ -3,15 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SynonymV3 {
     String word;
     StringBuilder pageData;
+    ArrayList<String> rawSynonyms;
+    ArrayList<String> synonyms;
     public SynonymV3(String word) {
         this.word = word;
         this.pageData = new StringBuilder();
+        this.rawSynonyms = new ArrayList<>();
         BufferedReader parser = null;
         HttpURLConnection connection = null;
         try {
@@ -35,10 +40,36 @@ public class SynonymV3 {
         Pattern pattern = Pattern.compile("data-linkid=\"y2woe7\".*?>(.*?)</a>");
         Matcher matcher = pattern.matcher(pageData.toString());
         while (matcher.find()) {
-            System.out.println(matcher.group(1));
+            this.rawSynonyms.add(matcher.group(1));
         }
+        this.normalizeData();
+    }
+    public void normalizeData() {
+        HashSet<String> listHashSet = new HashSet<>();
+        for (String rawSynonym : rawSynonyms) {
+            StringBuilder rawSynonymFormatted = new StringBuilder(rawSynonym);
+            String[] characterCodes = new String[]{"%20", "%27", "%2F", "&#x27;"};
+            for (String characterCode : characterCodes) {
+                String codeToChar = switch (characterCode) {
+                    case "%20" -> " ";
+                    case "%27" -> "'";
+                    case "%2F" -> "/";
+                    case "&#x27;" -> "\'";
+                    default -> "";
+                };
+                while (rawSynonymFormatted.toString().contains(characterCode)) {
+                    rawSynonymFormatted.insert(rawSynonym.indexOf(characterCode), codeToChar);
+                    rawSynonymFormatted.delete(rawSynonym.indexOf(characterCode) + 1, rawSynonymFormatted.indexOf(characterCode)
+                                                                                  + characterCode.length());
+                }
+            }
+            listHashSet.add(rawSynonymFormatted.toString());
+        }
+        this.synonyms = new ArrayList<>(listHashSet);
     }
     public static void main(String[] args) {
         SynonymV3 synonym = new SynonymV3("hello");
+        synonym.rawSynonyms.forEach(System.out::println);
+        synonym.synonyms.forEach(System.out::println);
     }
 }
